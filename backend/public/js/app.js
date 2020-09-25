573afa597f6319897a65399b45f2805ca306dec6
birdover1214 <fk12144473@gmail.com>
2020-09-25 11:37:04 +0900

    fontawesome

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /work/backend/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /work/backend/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\userpc\desktop\docker-php\laravel-tmp\backend\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\userpc\desktop\docker-php\laravel-tmp\backend\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
